d78be1fde49344a7e19c6f1647993d1f360f7925
Luis Enrique Tovar Ch <luistovar@cmg.mx>
2025-03-15 20:30:19 -0600

    .

--- obj/Debug/net8.0-android/MauiApp2.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/MauiApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+704435cf667878471104e7c4048df0a5223de0f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
